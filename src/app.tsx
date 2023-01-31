931d1e1417e1e61ac0f0128cd6d73cde46972a62
Digtyarenk0 <uhmeplay811@gmail.com>
2023-01-31 22:12:41 +0200

    5 lesson: scss module

---
+++ src/app.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Counter } from "./components/counter";
+import "./index.scss";
+
+export const App = () => {
+  return (
+    <div className="app">
+      <Counter />
+    </div>
+  );
+};
