2ea04e950ceab3cd03881299bbdbef509122b557
Digtyarenk0 <uhmeplay811@gmail.com>
2023-01-31 23:09:59 +0200

    7 lesson: add theme and styles

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,15 @@ import React, { Suspense } from "react";
 import { Link, Route, Routes } from "react-router-dom";
 import { AboutPageAsync } from "./pages/about/about.async";
 import { MainPageAsync } from "./pages/main/main.async";
-import "./index.scss";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
 
 export const App = () => {
+  const { theme, toggleTheme } = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
+      <button onClick={toggleTheme}>TOGGLE</button>
       <Link to={"/"}>Главная</Link>
       <Link to={"/about"}>О сайте</Link>
       <Suspense fallback={<div>Loading...</div>}>
