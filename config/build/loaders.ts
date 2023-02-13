b21c07dea8d6ad050c80b6078decceb3ba292820
Digtyarenk0 <uhmeplay811@gmail.com>
2023-02-14 00:50:00 +0200

    17: Babel loader

--- config/build/loaders.ts
+++ config/build/loaders.ts
@@ -26,6 +26,26 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
       "sass-loader",
     ],
   };
+
+  const babelLoader = {
+    test: /\.(js|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: "babel-loader",
+      options: {
+        presets: ["@babel/preset-env"],
+        plugins: [
+          [
+            "i18next-extract",
+            {
+              locales: ["ru", "en"],
+              keyAsDefaultValue: true,
+            },
+          ],
+        ],
+      },
+    },
+  };
   const typescriptLoader = {
     test: /\.tsx?$/,
     use: "ts-loader",
@@ -41,5 +61,5 @@ export function buildLoaders({ isDev }: BuildOptions): webpack.RuleSetRule[] {
     ],
   };
 
-  return [fileLoader, svgLoader, typescriptLoader, cssLoader];
+  return [fileLoader, svgLoader, babelLoader, typescriptLoader, cssLoader];
 }
