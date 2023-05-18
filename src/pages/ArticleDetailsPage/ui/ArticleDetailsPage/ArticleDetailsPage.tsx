ed6aff710ee5cf6032a167c61bd7d5c8a0cd5175
Digtyarenk0 <uhmeplay811@gmail.com>
2023-05-18 23:28:04 +0300

    48: Article page && public webpack url

---
+++ src/pages/ArticleDetailsPage/ui/ArticleDetailsPage/ArticleDetailsPage.tsx
@@ -0,0 +1,22 @@
+import { useTranslation } from 'react-i18next';
+import { memo } from 'react';
+import classNames from 'classnames';
+import cls from './ArticleDetailsPage.module.scss';
+
+interface ArticleDetailsPageProps {
+    className?: string;
+}
+
+const ArticleDetailsPage = (props: ArticleDetailsPageProps) => {
+    const { className } = props;
+    const { t } = useTranslation();
+
+    return (
+        // eslint-disable-next-line i18next/no-literal-string
+        <div className={classNames(cls.ArticleDetailsPage, {}, [className])}>
+            ARTICLE DETAILS
+        </div>
+    );
+};
+
+export default memo(ArticleDetailsPage);
