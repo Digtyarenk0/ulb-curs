1085c3e2b81233b180cb457a3018ba13b1e08fd5
Digtyarenk0 <uhmeplay811@gmail.com>
2023-06-04 00:36:06 +0300

    51: Add comments wit normolize

--- src/app/providers/StoreProvider/config/StateSchema.ts
+++ src/app/providers/StoreProvider/config/StateSchema.ts
@@ -7,6 +7,7 @@ import { ProfileSchema } from 'entities/Profile';
 import { AxiosInstance } from 'axios';
 import { NavigateOptions } from 'react-router';
 import { ArticleDetailsSchema } from 'entities/Article';
+import { ArticleDetailsCommentsSchema } from 'pages/ArticleDetailsPage';
 import { To } from 'react-router-dom';
 
 export interface StateSchema {
@@ -17,6 +18,7 @@ export interface StateSchema {
   loginForm?: LoginSchema;
   profile?: ProfileSchema;
   articleDetails?: ArticleDetailsSchema;
+  articleDetailsComments?: ArticleDetailsCommentsSchema;
 }
 
 export type StateSchemaKey = keyof StateSchema;
